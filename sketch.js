119604d38c1f0a66fe17252031dddf5033f2f426
Michael Ramón <michael.ramon@ucuenca.edu.ec>
2023-10-20 20:44:38 -0500

    Se trabajo el código en clases y se mejoro como deber.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+let posX;
+let posY;
+let vel;
+let diam;
+let rad;
+let esp;
+let margen;
+let piso;
+let acel;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  posX = windowWidth / 2;
+  posY = windowHeight * 0.2;
+  rectMode(CENTER);
+  diam = random(10, 50);
+  rad = diam / 2;
+  esp = 50;
+  margen = 40;
+  piso = windowHeight - margen - esp / 2;
+  vel = 0;
+  acel = 0.98;
+  //frameRate(10);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(255, 200, 150);
+
+  actualizar();
+
+  noStroke();
+  fill(255, 100, 0);
+  circle(posX, posY, 20);
+  fill(100, 50, 0);
+  rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
+}
+
+function actualizar() {
+  posY += vel;
+
+  if (posY > piso - rad) {
+    print("ya");
+    vel *= -1;
+    posY += vel;
+  }
+  vel += acel;
 }
